7eff203998075551e55b395b30cee5f8530bc910
bebehack3r <20886900+bebehack3r@users.noreply.github.com>
2024-03-11 23:40:42 +0800

    login page style done

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,10 +3,81 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import styled from "styled-components";
 
+import logo from "./logo.png";
+
 const Wrap = styled.div`
   display: flex;
   flex: 1;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const LogoBlock = styled.div`
+  display: flex;
   flex-direction: column;
+  border-radius: 15px;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  flex: 5;
+`;
+
+const LogoImage = styled.img`
+  height: 80%;
+  border-radius: 20px;
+`;
+
+const LoginBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  flex: 4;
+  color: white;
+`;
+
+const LoginDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border-radius: 15px;
+  background: #161414;
+  width: 40vw;
+  height: 50vh;
+  color: white;
+`;
+
+const LoginDivInner = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 80%;
+  font-size: 14pt;
+  text-align: center;
+`;
+
+const LabelField = styled.label`
+  text-align: left;
+`;
+
+const InputField = styled.input`
+  font-size: 14pt;
+  margin-bottom: 18px;
+  margin-top: 8px;
+  padding: 5px 10px;
+`;
+
+const SubmitButton = styled.input`
+  background: #8454F6;
+  border: none;
+  border-radius: 5px;
+  padding-top: 15px;
+  padding-bottom: 15px;
+  width: 50%;
+  margin-left: 25%;
+  font-size: 16pt;
+  color: black;
 `;
 
 const Login = () => {
@@ -40,6 +111,10 @@ const Login = () => {
     }
   };
 
+  const goSignUp = () => {
+    navigate("/register");
+  };
+
   useEffect(() => {
     if (token) {
       console.error('Token found');
@@ -49,9 +124,24 @@ const Login = () => {
 
   return(
     <Wrap>
-      <input type='text' id='login' name='login' onChange={handleEmail} placeholder="e-mail" />
-      <input type='password' id='password' name='password' onChange={handlePass} placeholder="password" />
-      <input type='submit' id='submit' name='submit' onClick={login} value="Log In" />
+      <LoginBlock>
+        <h1>Welcome to Dash!</h1>
+        <h1>Web-based IDS System</h1>
+        <LoginDiv>
+          <LoginDivInner>
+            <LabelField for='login'>E-mail</LabelField>
+            <InputField type='text' id='login' name='login' onChange={handleEmail} placeholder="Enter your e-mail" />
+            <LabelField for='password'>Password</LabelField>
+            <InputField type='password' id='password' name='password' onChange={handlePass} placeholder="Enter your password" />
+            <SubmitButton type='submit' id='submit' name='submit' onClick={login} value="Log In" />
+            <h3>–––––––––– or ––––––––––</h3>
+            <SubmitButton type='submit' id='submit' name='submit' onClick={goSignUp} value="Create New Account" />
+          </LoginDivInner>
+        </LoginDiv>
+      </LoginBlock>
+      <LogoBlock>
+        <LogoImage src={logo} />
+      </LogoBlock>
     </Wrap>
   );
 };
